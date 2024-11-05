e9f0c2b2f8332c46bd2e08f52bcfbeba7fff6103
bracedd <ytbraced@gmail.com>
2024-11-05 19:47:58 +0400

    Orientation Changes After Login

--- Desky/DeskyApp.swift
+++ Desky/DeskyApp.swift
@@ -1,13 +1,22 @@
 import SwiftUI
 
+
 @main
 struct DeskyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate // Use the custom AppDelegate
     @StateObject private var loginStatus = LoginStatus()
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(loginStatus)
+                .onChange(of: loginStatus.isLoggedIn) { isLoggedIn in
+                    // Lock orientation to landscape after login
+                    AppDelegate.orientationLock = isLoggedIn ? .landscape : .all
+                    if isLoggedIn {
+                        UIDevice.current.setValue(UIInterfaceOrientation.landscapeLeft.rawValue, forKey: "orientation")
+                    }
+                }
         }
     }
 }
